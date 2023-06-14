4496be9a353aeac4990e74d5b2bc88c26004f99f
neintoes <45003750+neintoes@users.noreply.github.com>
2023-06-14 03:17:38 +0100

    Guided Hack 2 - Problem on line 65 accessing animation frame

--- GameManager.ts
+++ GameManager.ts
@@ -11,7 +11,10 @@ class GameManager {
     private tileMap: TilemapManager;
     private tileMapLevels: tiles.TileMapData[];
     public gravity: number = 8;
-
+    // GH2
+    private torchFrame: number = 0;
+    private tilesToAnimate: tiles.Location[];
+    // end GH2
 
     constructor(tilemapsToLoad: tiles.TileMapData[]) {
         this.tileMapLevels = tilemapsToLoad;
@@ -31,6 +34,9 @@ class GameManager {
     private setupScene(): void {
         this.tileMap = new TilemapManager(this.tileMapLevels[this.level], this.me);
         this.tileMap.buildLevel();
+        // GH2
+        this.tilesToAnimate = tiles.getTilesByType(assets.tile`torch`);
+        // end GH2
     }
 
     private onUpdates(): void {
@@ -44,11 +50,26 @@ class GameManager {
     }
 
     private onUpdateIntervals(): void {
+        // bat spawning
         game.onUpdateInterval(2000, function(): void {
             let bat = new EnemySprite(assets.image`bat`);
             bat.positionBat(this.me.sprite);
             bat.handleMovement(this.me.sprite);
         })
+
+        // GH2
+        //torch flicker
+        // game.onUpdateInterval(200, function(): void {
+        //     let anim = assets.animation`torch flicker`;
+        //     this.tilesToAnimate.foreach(function(tile: tiles.Location){
+        //         tiles.setTileAt(tile, anim.get(this.torchFrame));
+        //     });
+        //     this.torchFrame += 1;
+        //     if (this.torchFrame == anim.length - 1) {
+        //         this.torchFrame = 0;
+        //     }
+        // })
+        // end GH2
     }
 
     private onOverlaps(): void {
